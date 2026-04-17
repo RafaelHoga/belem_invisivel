47a43320dd2c161044e6f83750ca0d44472c2d07
jv4863586-makere <jv4863586@gmail.com>
2026-04-17 12:05:06 -0300

    consertei alguns detalhes

--- js/tela-hoteis.js
+++ js/tela-hoteis.js
@@ -1,17 +1,3 @@
-let hamburguer = document.getElementById("hamburguer");
-let menu = document.getElementById("menu");
-
-hamburguer.addEventListener("click", function () {
-
-  menu.classList.toggle("active");
-
-  // Troca ícone
-  if (menu.classList.contains("active")) {
-    hamburguer.innerText = "✖";
-  } else {
-    hamburguer.innerText = "☰";
-  }
-});
 
 const carousel = document.getElementById("carouselImages");
 const slides = document.querySelectorAll(".slide");
