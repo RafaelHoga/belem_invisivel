febb7b99a5b4e06a90451d117f39590df3d45f1f
jv4863586-makere <jv4863586@gmail.com>
2026-05-11 11:49:56 -0300

    mapa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+ var map = L.map('map').setView([-1.45502, -48.5024], 13);
+
+ var marker = L.marker([1.455, -48.484]).addTo(map);
+
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 // FORMULÁRIO
 
 let form = document.getElementById("formContato");
@@ -23,3 +32,4 @@ form.addEventListener("submit", function (event) {
 
   form.reset();
 });
+
