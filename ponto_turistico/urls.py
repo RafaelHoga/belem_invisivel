a577103b451284322cacc4d7125791560b8c2091
Gabriel <gabriell6510@gmail.com>
2026-05-29 12:10:10 -0300

    tirei os htmls duplicados, e resolvi a questão do caminho dos hoteis e lugares_turisticos(referente ao index.html)

---
+++ ponto_turistico/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('hotel-ibis/', TemplateView.as_view(
+        template_name='hoteis/tela-hotel-ibis.html'
+    ), name='tela_hotel_ibis'),
+
+    path('hotel-ipe/', TemplateView.as_view(
+        template_name='hoteis/tela-hotel-ipe.html'
+    ), name='tela_hotel_ipe'),
+
+    path('hotel-soft/', TemplateView.as_view(
+        template_name='hoteis/tela-hotel-soft.html'
+    ), name='tela_hotel_soft'),
+    path('estacao-docas/', TemplateView.as_view(
+        template_name='lugares_turisticos/tela-estacao-docas.html'
+    ), name='tela_estacao_docas'),
+    path('ilha-cotijuba/', TemplateView.as_view(
+        template_name='lugares_turisticos/tela-ilha-de-cotijuba.html'
+    ), name='tela_ilha_cotijuba'),
+]
