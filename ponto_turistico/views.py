70abea780e3016a2ec8464e3821d17b19aa4bf6d
jv4863586-makere <jv4863586@gmail.com>
2026-06-10 11:42:05 -0300

    telas

--- ponto_turistico/views.py
+++ ponto_turistico/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def hotel_ibis(request):
     return render(request, 'hoteis/tela-hotel-ibis.html')
+
+def utinga(request):
+    return render(request, 'lugares_turisticos/tela-utinga.html')
