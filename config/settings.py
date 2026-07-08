d439e3e25625896be9da9744baf8bf132b83424b
rafael <hogayuudyi@gmail.com>
2026-07-08 14:55:26 -0300

    avaliacao terminei

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ else:
             "ENGINE": "django.db.backends.mysql",
             "NAME": config("DB_NAME", default="belem_invisivel"),
             "USER": config("DB_USER", default="root"),
-            "PASSWORD": config("DB_PASSWORD", default=""),
+            "PASSWORD": config("DB_PASSWORD", default="123456"),
             "HOST": config("DB_HOST", default="127.0.0.1"),
             "PORT": config("DB_PORT", default="3306"),
         }
